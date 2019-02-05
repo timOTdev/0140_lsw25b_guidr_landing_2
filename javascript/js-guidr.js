bd20162207247abdc4cb5435f2bde0f3fbdbec85
Jamie Goodnight <goodnightlambda@gmail.com>
2019-02-05 13:31:51 -0500

    JavaScript added, styling tweaks, copy tweaks

---
+++ javascript/js-guidr.js
@@ -0,0 +1,32 @@
+
+
+const zero=document.querySelector(".zero");
+const two=document.querySelector(".two");
+const four=document.querySelector(".four");
+
+  $(document).ready(() => {
+  $(zero).fadeIn(3000);
+  $(two).fadeIn(2500);
+  $(four).fadeIn(2700);
+  $(".fa-campground").fadeIn(300);
+  $(".fa-hiking").fadeIn(700);
+  $(".fa-mountain").fadeIn(1100);
+  $(".fa-binoculars").fadeIn(1500);
+  $("h1").fadeIn(3000);
+  $(".site-icons").fadeIn(1100);
+  $(".fa-compass").fadeIn(1500);
+  });
+
+const heading = document.querySelector("h1");
+const container=document.querySelector(".container");
+
+
+
+// TweenMax.to(heading, 2, { ease: Power4.easeOut, x: 600 });
+
+
+
+
+// TweenLite.to(".site-icons", 2, { ease: Power4.easeOut, x: 320 });
+TweenMax.to(container, 2.5, { ease: Power3.easeOut, x: 600 });
+
